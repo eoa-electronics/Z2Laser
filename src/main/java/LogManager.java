5312cf91943390a0e213e408de312c49af331925
EOA-electronics <info@eoa-electronics.de>
2022-02-17 13:29:28 +0100

    implemented auto-update of log in UI

--- src/main/java/LogManager.java
+++ src/main/java/LogManager.java
@@ -4,14 +4,21 @@ import java.util.List;
 public class LogManager {
     //attributes
     private List<LogEntry> log;
+    private AppUI ui;
 
     //constructor
-    public LogManager() {
+    public LogManager(AppUI appui) {
         log = new ArrayList<LogEntry>();
+        ui = appui;
+    }
+
+    private void updateAppUI() {
+        ui.updateLog(printLog());
     }
 
     public void addEntry(String entryText) {
         log.add(new LogEntry(entryText));
+        updateAppUI();
     }
 
     public String printLog() {
